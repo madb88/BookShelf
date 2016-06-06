223e55db0d69edba18ac31d66dcb3aa671409ca0
madb88 <kaminskiqba@gmail.com>
2016-06-06 14:07:49 +0200

    Delete test.php

--- api/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if ($_SERVER['REQUEST_METHOD'] === 'DELETE'){
-    var_dump($_SERVER);
-//    parse_str(file_get_contents("php://input"), $del_vars);
-//    var_dump($del_vars);
-//    var_dump($del_vars['id']);
-}
