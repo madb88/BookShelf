e48e76c9e364d0dfa138d52eb1709b41be5f210c
madb88 <kaminskiqba@gmail.com>
2016-06-12 10:32:46 +0200

    feedback update

--- api/books.php
+++ api/books.php
@@ -1,25 +1,24 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
 require_once ('./src/connection.php');
 require_once ('./src/book.php');
 
 
-if($_SERVER['REQUEST_METHOD'] === "GET"){
-    if(isset($_GET['id'])){
+if ($_SERVER['REQUEST_METHOD'] === "GET") {
+    if (isset($_GET['id'])) {
         $bookToShow = new Book();
         $bookToShow->loadBookFromDb($_GET['id'], $conn);
-        $bookToShowJSON=  json_encode($bookToShow->toArray());
+        $bookToShowJSON = json_encode($bookToShow->toArray());
         echo($bookToShowJSON);
-        
-    } 
-        else {
-            $allBooksName = Book::getBooksNames($conn);
-            $allBooksNameJSON = json_encode($allBooksName);
-            echo $allBooksNameJSON;
-        }
+    } else {
+        $allBooksName = Book::getBooksNames($conn);
+        $allBooksNameJSON = json_encode($allBooksName);
+        echo $allBooksNameJSON;
+    }
 }
-    
-if ($_SERVER['REQUEST_METHOD'] === "POST"){
+
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
     $bookToCreate = new Book();
     $bookToCreate->setTitle($_POST['title']);
     $bookToCreate->setAuthor($_POST['author']);
@@ -27,25 +26,21 @@ if ($_SERVER['REQUEST_METHOD'] === "POST"){
     $bookToCreate->saveBookToDb($conn);
     $bookToCreateJSON = json_encode($bookToCreate);
     echo ($bookToCreateJSON);
-    
 }
-if($_SERVER['REQUEST_METHOD'] === 'DELETE'){
-    
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
     parse_str(file_get_contents("php://input"), $del_vars);
-    
+
     $bookToDelete = new Book();
-    $bookToDelete->loadBookFromDb($del_vars['id'], $conn);
+    $bookToDelete->loadBookFromDb($conn, $del_vars['id']);
     $bookToDelete->deleteFromDb($conn, $del_vars['id']);
-//    echo json_encode($bookToDelete);
-
-
+    echo json_encode($bookToDelete);
 }
 
-if($_SERVER['REQUEST_METHOD'] === 'PUT'){
-    
+if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+
     parse_str(file_get_contents("php://input"), $put_vars);
     $bookToEdit = new Book();
     $bookToEdit->update($conn, $put_vars['id'], $put_vars['title'], $put_vars['author'], $put_vars['description']);
     echo json_encode($bookToEdit);
-    
 }
--- api/src/book.php
+++ api/src/book.php
@@ -1,101 +1,99 @@
 <?php
+
 require_once 'connection.php';
 
 header("Access-Control-Allow-Origin: *");
 
-class Book{
-    
-    static public function getBooksNames(mysqli $conn){
+class Book {
+
+    static public function getBooksNames(mysqli $conn) {
         $ret = [];
         $sql = "SELECT id, name FROM Books";
-        $result = $conn-> query($sql);
-        if($result != false){
-            if($result->num_rows >0){
-                while($row = $result->fetch_assoc()){
+        $result = $conn->query($sql);
+        if ($result != false) {
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
                     $ret[] = $row;
                 }
             }
         }
-        
+
         return $ret;
     }
-    
+
     private $id;
     private $title;
     private $author;
     private $description;
-    
-    public function __construct(){
+
+    public function __construct() {
         $this->id = -1;
         $this->title = "";
         $this->author = "";
         $this->description = "";
     }
-    
-    public function getId(){
+
+    public function getId() {
         return $this->id;
     }
-    
-    public function setTitle($newTitle){
+
+    public function setTitle($newTitle) {
         $this->title = $newTitle;
     }
-    
-    public function getTitle(){
+
+    public function getTitle() {
         return $this->title;
     }
-    
-    public function setAuthor($newAuthor){
+
+    public function setAuthor($newAuthor) {
         $this->author = $newAuthor;
     }
-    
-    public function getAuthor(){
+
+    public function getAuthor() {
         return $this->author;
     }
-    
-    public function setDescription($newDescription){
+
+    public function setDescription($newDescription) {
         $this->description = $newDescription;
     }
-    
-    public function getDescription(){
+
+    public function getDescription() {
         return $this->description;
     }
-    
-    public function deleteFromDb(mysqli $conn, $id){
+
+    public function deleteFromDb(mysqli $conn, $id) {
         $sql = "DELETE FROM Books WHERE id=$id";
         $result = $conn->query($sql);
         return $result;
     }
-      
-       
-    
-    public function saveBookToDb(mysqli $conn){
-        if($this->id === -1){
+
+    public function saveBookToDb(mysqli $conn) {
+        if ($this->id === -1) {
             $sql = "INSERT INTO Books (name, author_name, description) VALUES 
                     ('{$this->title}', '{$this->author}', '{$this->description}')";
             $result = $conn->query($sql);
-            if($result == true){
+            if ($result == true) {
                 $this->id = $conn->insert_id;
                 return true;
             }
             return false;
-        } 
+        }
     }
-    
-    public function update(mysqli $conn, $id, $name, $author, $description){
+
+    public function update(mysqli $conn, $id, $name, $author, $description) {
         $sql = "UPDATE Books SET name = '$name', author_name = '$author', description = '$description'
                 WHERE id = $id";
         $result = $conn->query($sql);
         return $result;
     }
-       
-    
-    public function loadBookFromDb($id, mysqli $conn){
+
+    public function loadBookFromDb(mysqli $conn, $id) {
         $sql = "SELECT * FROM Books WHERE id = {$id}";
         $result = $conn->query($sql);
-        if($result != false){
-            if($result->num_rows === 1){
+        if ($result != false) {
+            if ($result->num_rows === 1) {
                 $row = $result->fetch_assoc();
-                $this->id = (int)$row['id'];
+                $this->id = (int) $row['id'];
                 $this->title = $row['name'];
                 $this->author = $row['author_name'];
                 $this->description = $row['description'];
@@ -104,16 +102,16 @@ class Book{
         }
         return false;
     }
-    
-    
-    public function toArray(){
+
+    public function toArray() {
         $ret = [];
-        
+
         $ret['id'] = $this->id;
         $ret['title'] = $this->title;
         $ret['author'] = $this->author;
         $ret['description'] = $this->description;
-        
+
         return $ret;
     }
+
 }
